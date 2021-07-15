59a58cc9a715164da21e6f9650b40660ce9dab4a
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-07-15 13:07:18 +0200

    test: 💍 Tests fixed

--- src/charts/bar/transform.test.ts
+++ src/charts/bar/transform.test.ts
@@ -3,10 +3,20 @@ import transform, { PartialBarChartSettings } from './transform';
 test('transforms bar chart settings to customization interface', () => {
   const chartSettings = {
     yScaleSettings: { type: 'linear', formatLabel: null },
+    theme: {
+      gridX: {
+        enabled: true,
+      },
+      gridY: {
+        enabled: true,
+      },
+    },
   } as PartialBarChartSettings;
 
   expect(transform.serializeIn(chartSettings)).toEqual({
     formatValue: null,
+    horizontalGrid: true,
+    verticalGrid: true,
   });
 });
 
@@ -16,20 +26,46 @@ test('transforms bar chart scale label format settings to customization interfac
       type: 'linear',
       formatLabel: '${number; 0.00; multiply; 100}£',
     },
+    theme: {
+      gridX: {
+        enabled: true,
+      },
+      gridY: {
+        enabled: true,
+      },
+    },
   } as PartialBarChartSettings;
 
   expect(transform.serializeIn(chartSettings)).toEqual({
     formatValue: '${number; 0.00; multiply; 100}£',
+    horizontalGrid: true,
+    verticalGrid: true,
   });
 });
 
 test('transforms customization interface to bar settings ', () => {
   const settings = {
     formatValue: '${number; 0.00}$',
+    theme: {
+      gridX: {
+        enabled: true,
+      },
+      gridY: {
+        enabled: true,
+      },
+    },
   };
 
   expect(transform.serializeOut(settings)).toMatchInlineSnapshot(`
     Object {
+      "theme": Object {
+        "gridX": Object {
+          "enabled": undefined,
+        },
+        "gridY": Object {
+          "enabled": undefined,
+        },
+      },
       "tooltipSettings": Object {
         "formatValue": "\${number; 0.00}$",
       },
