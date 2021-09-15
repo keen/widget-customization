f23c94bbe8bc6e056a78312ada64f4e35fd41da2
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-09-15 21:28:02 +0200

    fix: 🐛 Fix

--- src/components/ComponentSettings/ComponentSettings.tsx
+++ src/components/ComponentSettings/ComponentSettings.tsx
@@ -80,7 +80,7 @@ const ComponentSettings: FC<Props> = ({
   const { hiddenOptions } = componentSettingsConfig;
 
   const settingsComponent = getSettingsComponent(widgetType);
-  const settingsAreAvailable = !hiddenOptions?.card || widgetType === 'table';
+  const settingsAreAvailable = !hiddenOptions?.card || widgetType !== 'table';
 
   return settingsComponent && settingsAreAvailable ? (
     <MousePositionedTooltip
